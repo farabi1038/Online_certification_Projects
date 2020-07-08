6b420925f4c6fbd8cd301dcdd09a207eaf88fee8
Ibne Farabi Shihab <ibnfarabishihab@gmail.com>
2020-07-08 04:35:07 -0400

    Update run.py

--- Data Scientist Nano Degree/Disaster Response/app/run.py
+++ Data Scientist Nano Degree/Disaster Response/app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('Message', engine)
 
 # load model
-model = joblib.load("classifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
